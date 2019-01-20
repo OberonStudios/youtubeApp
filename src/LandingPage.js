a84160fb5813c2736651544e19dfbb78f677a41d
Angel Contreras <angelcbenoit@gmail.com>
2019-01-20 11:06:44 -0800

    add react player

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import ReactPlayer from 'react-player'
+
 
 class LandingPage extends Component {
     render() {
         return (
             <div>
-                HI
+                <ReactPlayer
+                    url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
+                    width={500}
+                    height={500}
+                />
             </div>
         );
     }
